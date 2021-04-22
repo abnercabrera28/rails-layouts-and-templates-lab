ed2b65dd0c18c0f4dfe5b828af89af0a8372a969
Abner <abnercabrera28@gmail.com>
2021-04-22 12:50:08 -0400

    commit tree 079bda7043bb9ffa92a1ba7059b1ace0f81e438f

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,12 @@
+class StoreAdminController < ApplicationController
+    layout "order_administration"
+    
+    def home
+        render layout: "admin"
+    end
+
+    def invoice
+        render layout: false
+    end
+
+end
